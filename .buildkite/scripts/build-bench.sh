7cb936288bf6e9b8539170e3b7a02cd2d6cccbad
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-12 15:23:35 -0700

    Update build-bench.sh

--- .buildkite/scripts/build-bench.sh
+++ .buildkite/scripts/build-bench.sh
@@ -2,25 +2,17 @@
 
 set -e
 here=$(dirname "$0")
-
 # shellcheck source=.buildkite/scripts/common.sh
 source "$here"/common.sh
-
-agent="${1-solana}"
-
+agent="${1-solana}
 build_steps() {
   cat <<EOF
-{
-  "name": "$1",
-  "command": "$2",
-  "timeout_in_minutes": 30,
-  "agent": "$agent",
-  "retry": 3
-}
-EOF
-}
-
+{ "name": "$1","command": "$2",
+  "timeout_in_minutes": 30, agent": "$agent",
+  "retry": 3 }
+EOF }
 # shellcheck disable=SC2016
-group "bench" \
-  "$(build_steps "bench-part-1" "ci/docker-run-default-image.sh ci/bench/part1.sh")" \
-  "$(build_steps "bench-part-2" "ci/docker-run-default-image.sh ci/bench/part2.sh")"
+group "bench
+\
+$(build_steps "bench-part-1" "ci/docker-run-default-image.sh ci/bench/part1.sh")" \
+ $(build_steps "bench-part-2" "ci/docker-run-default-image.sh ci/bench/part2.sh")"
